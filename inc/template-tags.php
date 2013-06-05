aca2a6ae7099d1b0ad2ccf34274992966d77a366
Srinivasan G <srinig112@gmail.com>
2013-06-05 11:55:57 +0530

    Minor improvements/fixes in comment-reply/cancel-reply links

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -105,9 +105,7 @@ function emphaino_comment( $comment, $args, $depth ) {
 
 			<div class="comment-content"><?php comment_text(); ?></div>
 
-			<div class="reply icon-reply">
-				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
+			<div class="reply icon-reply"><?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?></div><!-- .reply -->
 		</article><!-- #comment-## -->
 
 	<?php
