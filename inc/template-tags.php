f33d678e0f4feab08e3d8c53f403c113e80181e2
Srinivasan G <srinig112@gmail.com>
2013-09-02 11:51:10 +0530

    Minor fixes

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -105,7 +105,14 @@ function emphaino_comment( $comment, $args, $depth ) {
 
 			<div class="comment-content"><?php comment_text(); ?></div>
 
-			<div class="reply icon-reply"><?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?></div><!-- .reply -->
+			<?php
+				comment_reply_link( array_merge( $args, array(
+					'depth' => $depth,
+					'max_depth' => $args['max_depth'],
+					'before' => '<div class="reply icon-reply">',
+					'after' => '</div>',
+				) ) );
+			?>
 		</article><!-- #comment-## -->
 
 	<?php
