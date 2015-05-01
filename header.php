471095db5a3535a02be533d87a1cc0d3645317b8
Srini Guna <s@srinig.com>
2015-05-01 11:43:23 +0530

    Removed unnecessary code from header.php

--- header.php
+++ header.php
@@ -60,12 +60,6 @@
 			</div>
 		</div>
 
-<!-- 		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
-		<?php endif; ?>
- -->
-
 	</header><!-- #masthead .site-header -->
 
 	<div id="main" class="site-main">
